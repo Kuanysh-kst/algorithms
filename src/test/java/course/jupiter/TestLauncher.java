faa45ffa4e94c63c6a0a6d9ca904553595b8e75f
Kuanysh <kuanysh4646@gmail.com>
2023-03-13 18:58:51 +0600

    add launcher
    Signed-off-by: Kuanysh <kuanysh4646@gmail.com>

---
+++ src/test/java/course/jupiter/TestLauncher.java
@@ -0,0 +1,27 @@
+package course.jupiter;
+
+import org.junit.platform.engine.DiscoverySelector;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+    public static void main(String[] args) {
+        var launcher = LauncherFactory.create();
+
+        var summaryGeneratingListener = new SummaryGeneratingListener();
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
+                .request()
+                .selectors(DiscoverySelectors.selectPackage("course.jupiter"))
+                .build();
+        launcher.execute(request, summaryGeneratingListener);
+
+        try (var writer = new PrintWriter(System.out)) {
+            summaryGeneratingListener.getSummary().printTo(writer);
+        }
+    }
+}
